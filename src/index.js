db2fa319f9141705a3fdfde5cf185bed8df0286a
shmilky <shmilkykup@gmail.com>
2016-08-22 12:16:46 +0300

    Add pie and doughnut charts support

--- src/index.js
+++ src/index.js
@@ -6,74 +6,82 @@ var PropTypes= React.PropTypes;
 // import ReactDOM from 'react-dom';
 var Chart = require('chart.js');
 
-module.exports = React.createClass({
-    displayName: 'LineChart',
+var createChartClassByType = function(chartType) {
+    return React.createClass({
+        displayName: 'LineChart',
 
-    propTypes: {
-        chartId: PropTypes.string.isRequired,
-        chartData: PropTypes.object.isRequired,
-        legend: PropTypes.object,
-        options: PropTypes.object,
-        redraw: PropTypes.bool
-    },
+        propTypes: {
+            chartId: PropTypes.string.isRequired,
+            chartData: PropTypes.object.isRequired,
+            legend: PropTypes.object,
+            options: PropTypes.object,
+            redraw: PropTypes.bool
+        },
 
-    getDefaultProps() {
-        return {
-            redraw: false
-        };
-    },
+        getDefaultProps() {
+            return {
+                redraw: false
+            };
+        },
 
-    componentWillMount() {
-        this.chart_instance = undefined;
-    },
+        componentWillMount() {
+            this.chart_instance = undefined;
+        },
 
-    componentDidMount() {
-        this.renderChart();
-    },
-
-    componentDidUpdate() {
-        if (this.props.redraw) {
-            this.chart_instance.destroy();
+        componentDidMount() {
             this.renderChart();
-        } else {
-            this.updateChart();
+        },
+
+        componentDidUpdate() {
+            if (this.props.redraw) {
+                this.chart_instance.destroy();
+                this.renderChart();
+            } else {
+                this.updateChart();
+            }
+        },
+
+        componentWillUnmount() {
+            this.chart_instance.destroy();
+        },
+
+        updateChart() {
+            const {chartData, chartOptions} = this.props;
+
+            if (!this.chart_instance) return;
+
+            if (chartOptions) {
+                Chart.helpers.configMerge(this.chart_instance.options, chartOptions);
+            }
+
+            this.chart_instance.data.datasets = chartData.datasets;
+            this.chart_instance.data.labels = chartData.labels;
+
+
+            this.chart_instance.update();
+        },
+
+        renderChart() {
+            const {chartData, chartId, chartOptions, globalConfig} = this.props;
+
+            Object.assign(Chart.defaults.global, globalConfig);
+
+            var ctx = document.getElementById(chartId).getContext("2d");
+            this.chart_instance = new Chart(ctx, {
+                type: chartType,
+                data: chartData,
+                options: chartOptions
+            });
+        },
+
+        render() {
+            return React.createElement('canvas', { style: { width: '100%', height: '100%' }, id: this.props.chartId });
         }
-    },
+    });
+};
 
-    componentWillUnmount() {
-        this.chart_instance.destroy();
-    },
+module.exports.LineChart = createChartClassByType('line');
 
-    updateChart() {
-        const {chartData, chartOptions} = this.props;
+module.exports.PieChart = createChartClassByType('pie');
 
-        if (!this.chart_instance) return;
-
-        if (chartOptions) {
-            Chart.helpers.configMerge(this.chart_instance.options, chartOptions);
-        }
-
-        this.chart_instance.data.datasets = chartData.datasets;
-        this.chart_instance.data.labels = chartData.labels;
-
-
-        this.chart_instance.update();
-    },
-
-    renderChart() {
-        const {chartData, chartId, chartOptions, globalConfig} = this.props;
-
-        Object.assign(Chart.defaults.global, globalConfig);
-
-        var ctx = document.getElementById(chartId).getContext("2d");
-        this.chart_instance = new Chart(ctx, {
-            type: 'line',
-            data: chartData,
-            options: chartOptions
-        });
-    },
-
-    render() {
-        return React.createElement('canvas', { style: { width: '100%', height: '100%' }, id: this.props.chartId });
-    }
-});
+module.exports.DoughnutChart = createChartClassByType('doughnut');
